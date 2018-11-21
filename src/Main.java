13c36854cbb3e3bd95ad0c6bae873b9862f62638
Jeff Jung <8jeffnine@gmail.com>
2018-11-21 23:15:00 +0900

    base

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        int test_code=  5;
+        for(int i=0; i<test_code; i++) System.out.println("#"+i);
+    }
+}
